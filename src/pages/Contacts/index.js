848b5fa80462450208c742eead0c6591b2ace4cf
Artur Rodrigues <artur190503@outlook.com>
2023-12-04 14:16:10 -0300

    Iniciando criação dinâmica de agenda

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -3,11 +3,36 @@ import { View, Text } from 'react-native';
 
 export default function Contacts({navigation}){
     return(
-        <View>
-            <Text>Contacts!</Text>
-            <Text
-            onPress={() => navigation.navigate('Information')}
-            >Information...</Text>
+        <View style={{marginLeft: 30}}>
+            <View style={{marginTop: 20}}>
+                <Text>Nome: Artur Rodrigues</Text>
+                <Text>Telefone: (61) 999999999</Text>
+                <Text
+                onPress={() => navigation.navigate('Information',
+                {
+                    nome: 'Artur Rodrigues',
+                    dataNascimento: '19/05/2003',
+                    telefone: '(61) 999999999',
+                    email: 'artur190503@outlook.com',
+                    ocupacao: 'Estudante',
+                })}
+                >Information...</Text>
+            </View>
+            <View style={{marginTop: 20}}>
+                <Text>Nome: Alice Reis</Text>
+                <Text>Telefone: (61) 988888888</Text>
+                <Text
+                onPress={() => navigation.navigate('Information',
+                
+                {
+                    nome: 'Alice Reis',
+                    dataNascimento: '11/10/2003',
+                    telefone: '(61) 988888888',
+                    email: 'alicelinda@yahoo.com',
+                    ocupacao: 'Estudante',
+                })}
+                >Information...</Text>
+            </View>
         </View>
     )
 }
--- src/pages/Information/index.js
+++ src/pages/Information/index.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { StyleSheet } from 'react-native';
 
-export default function Information(){
+export default function Information({route}){
     return(
-        <View>
-            <Text style={styles.container}>Information!</Text>
+        <View style={{marginTop: 30, marginLeft: 30}}>
+            <Text>Nome: {route.params?.nome}</Text>
+            <Text>Data de Nascimento: {route.params?.dataNascimento}</Text>
+            <Text>Telefone: {route.params?.telefone}</Text>
+            <Text>E-mail: {route.params?.email}</Text>
+            <Text>Ocupação: {route.params?.ocupacao}</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        color: '#f123f4',
-    }
-})
-
